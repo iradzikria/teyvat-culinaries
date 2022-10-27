664609efa501ab9a1f88dae6e4f8417cccf8379b
iradzikria <107542805+iradzikria@users.noreply.github.com>
2022-10-27 14:17:50 +0700

    Add files via upload

---
+++ dicoding web/main.js
@@ -0,0 +1,39 @@
+var divs = ["Mond", "Liy", "Ina", "Sume"];
+var visibleDivId = null;
+function toggleVisibility(divId) {
+  if (visibleDivId === divId) {
+    visibleDivId = null;
+  } else {
+    visibleDivId = divId;
+  }
+  hideNonVisibleDivs();
+}
+function hideNonVisibleDivs() {
+  var i, divId, div;
+  for (i = 0; i < divs.length; i++) {
+    divId = divs[i];
+    div = document.getElementById(divId);
+    if (visibleDivId === divId) {
+      div.style.display = "block";
+    } else {
+      div.style.display = "none";
+    }
+  }
+}
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  slides[slideIndex - 1].style.display = "block";
+  setTimeout(showSlides, 3000);
+}
